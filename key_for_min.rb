efb7dba60e9bf6c8fbdfb97ea1f6cf4229b18b0d
Sean LaFlam <sean.laflam@gmail.com>
2020-08-18 19:44:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if low == nil 
       low = value
+      lowest = key
     elsif value < low
       low = value
       lowest = key
